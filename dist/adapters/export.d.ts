000c178b466308aa11674c7e5a4f3d5538d8ed5e
Mark Maher <100785809+Marco5dev@users.noreply.github.com>
2024-04-02 13:50:28 +0200

    Reverted some changes

--- dist/adapters/export.d.ts
+++
@@ -1,4 +0,0 @@
-import { jsonAdapter } from "./json.adapter";
-import { yamlAdapter } from "./yaml.adapter";
-import { sqlAdapter } from './sql.adapter';
-export { jsonAdapter, yamlAdapter, sqlAdapter };
--- dist/types/jsonverse.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=jsonverse.types.js.map
